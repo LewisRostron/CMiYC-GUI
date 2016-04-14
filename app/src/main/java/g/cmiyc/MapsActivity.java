d618ae78b25530fbefb5290074d7597c284045ce
Liefdrag <adamhobbs123@gmail.com>
2016-04-14 11:54:37 +0100

    Redesign using Lewis' styles.
    Added in most of the XML pages where there is an activity for each xml
    file. There is also swipe functionality for the in game display. Text
    boxes have been used to indicate additional components and where they
    will need to be placed.

--- app/src/main/java/g/cmiyc/MapsActivity.java
+++ app/src/main/java/g/cmiyc/MapsActivity.java
@@ -1,17 +1,12 @@
-package catchmeifyoucan.catchmeifyoucan;
+package g.cmiyc;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+public class MapsActivity extends FragmentActivity /*implements OnMapReadyCallback*/ {
+/*
     private GoogleMap mMap;
 
     @Override
@@ -43,6 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    /*
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -51,5 +47,5 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+    }*/
 }
